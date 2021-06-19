7d64b56f4ad589d28c6194d1353cea068e8d1cc4
PareshDevlekar <78142905+PareshDevlekar@users.noreply.github.com>
2021-06-19 15:33:56 +0530

    Initial Commit

---
+++ Admin/top.php
@@ -0,0 +1,16 @@
+	 <div class="header-top">
+		  <div class="wrapper">
+			 <div class="top-nav">
+				<div class="logo">
+					<a href="#"><img src="../images/logo.png" class="img-responsive" alt=""/></a>
+				</div>
+				<div class="menu">
+				<ul id="nav">
+					 <li><a href="../index.php" target="_blank">Preview Website</a></li>
+					 <li><a href="logout.php">Log Out</a></li>
+				     <div class="clearfix"></div>
+				 </ul>
+				 </div>
+			 </div>
+		 </div>
+	 </div>
